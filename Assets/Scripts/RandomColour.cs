c3608ea2c958b96ffaf251bec1ed99713276f013
archrofe <archrofe@gmail.com>
2018-02-21 11:56:38 +1100

    Change Colour

---
+++ Assets/Scripts/RandomColour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColour : MonoBehaviour
+{
+    public Material[] mats = null;
+    public Renderer rend;
+
+    // Use this for initialization
+    void Start()
+    {
+        RandomMat();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void RandomMat()
+    {
+        int randomMatIndex = Random.Range(0, 3);
+        rend = GetComponent<Renderer>();
+        rend.material = mats[randomMatIndex];
+    }
+}
